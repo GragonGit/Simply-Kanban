9c25d21073c3e0259b70e1c67811b52e90903648
Gragon <gragonator02@gmail.com>
2026-07-18 00:32:05 +0200

    Added Imprint

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
 <template>
-  <NuxtPage />
+<NuxtPage />
+<a href="https://gragongit.github.io/Info/">{{ $t('app.imprint') }}</a>
 </template>
+
+<style lang="sass" scoped>
+a
+  position: fixed
+  bottom: 0
+  right: 0
+
+  margin: 0.25rem
+
+  font-size: 6pt
+</style>
